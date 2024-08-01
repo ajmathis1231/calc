ae2a6c89ed620f9dee9ba566299c03ddfa798baf
ajmathis1231 <ajmathis1231@gmail.com>
2024-08-01 11:30:03 -0500

    Fully functioning calculator, added a "storage" digit that shows the operand that is going to be worked on by the next input so once you press an operator, you don't have to remember what was there.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,38 +11,78 @@ import { NgFor } from '@angular/common';
 })
 
 export class AppComponent {
-  title = 'calc';
-  Num1Input: number = 1;
-  Num2Input: number = 2;
-  Num3Input: number = 3;
-  Num4Input: number = 4;
-  Num5Input: number = 5;
-  Num6Input: number = 6;
-  Num7Input: number = 7;
-  Num8Input: number = 8;
-  Num9Input: number = 9;
-  Num0Input: number = 0;
+  
   displayedDigits: string = '0';
-/*      REMOVE COMMENT IF THE NEXT ATTEMPT DOESN'T WORK
-  setProperty(event: Event){
-    let target = event.target as HTMLInputElement
-    let property: string = target.id
-    switch(property){
-      case "Num1Input":
-        this.displayedDigits.push(this.Num1Input);
-        break;
-      case "Num2Input":
-        this.displayedDigits.push(this.Num1Input);
-        break;
+  firstOperand: number | null = null;
+  operator: string | null = null;
+  result: string | null = null;
+  calculatedResult: number | null = null;
+  storage: string = '0';
 
-    }
-        */
 setProperty(event: Event) {
   const buttonValue = (event.target as HTMLButtonElement).textContent;
   if (buttonValue){
-    const digit = parseInt(buttonValue, 10);
-    this.displayedDigits += digit.toString();
+    const value = buttonValue.trim();
+    if (value === 'C') {
+      this.displayedDigits = '0';
+      this.firstOperand = null;
+      this.operator = null;
+      this.result =null;
+      this.storage = '0';
+      return;
+    }
+    if (this.displayedDigits === '0') {
+      this.displayedDigits = '';
+    }
+    if (!isNaN(Number(value))) {
+      this.displayedDigits += value;
+    } else {
+      this.handleOperator(value);
+    } 
   }
 }
+  handleOperator(operator: string) {
+    
+    if (this.firstOperand === null) {
+      this.firstOperand = Number(this.displayedDigits);
+      this.storage = String(this.firstOperand);
+    } else if (this.operator != null && this.firstOperand !== null) {
+      switch (this.operator) {
+        case '+':
+          this.calculatedResult = this.firstOperand + Number(this.displayedDigits);
+          this.storage = String(this.firstOperand);
+          break;
+        case '-':
+          this.calculatedResult = this.firstOperand - Number(this.displayedDigits);
+          break;
+        case '*':
+          this.calculatedResult = this.firstOperand * Number(this.displayedDigits);
+          break;
+        case '/':
+          this.calculatedResult = this.firstOperand / Number(this.displayedDigits);
+          break;
+        case 'C':
+          this.displayedDigits = '0';
+          //this.firstOperand = null;
+          //this.operator = null;
+          break;
+          
+        
+      }   
+      this.result = String(this.calculatedResult);
+      
+    }
+    
+    if (operator ==='=' && this.result !== null) {
+      this.displayedDigits = this.result;
+      this.firstOperand = this.calculatedResult;
+      this.storage = this.displayedDigits;
+    } else {
+      this.operator = operator;
+      this.displayedDigits = '0';
+      //this.storage = '0' // clears the display for the next number
+    }
+    this.calculatedResult = null;
+  }
 }
 
